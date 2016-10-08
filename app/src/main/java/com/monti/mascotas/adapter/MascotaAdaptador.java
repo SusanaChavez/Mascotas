0edfaf45710d87e5c64f0d001d2f5d2267d27fe7
SusanaChavez <schavez@iinfo.unsj.edu.ar>
2016-10-08 11:08:06 -0300

    Add files via upload

---
+++ app/src/main/java/com/monti/mascotas/adapter/MascotaAdaptador.java
@@ -0,0 +1,65 @@
+package com.monti.mascotas.adapter;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.monti.mascotas.pojo.Mascota;
+import com.monti.mascotas.R;
+
+import java.util.ArrayList;
+
+public class MascotaAdaptador extends RecyclerView.Adapter<MascotaAdaptador.MascotaViewHolder>  {
+
+    ArrayList<Mascota> mascotas;
+    public MascotaAdaptador(ArrayList<Mascota> mascotas){
+        this.mascotas = mascotas;
+    }
+    @Override
+    public MascotaAdaptador.MascotaViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_mascota, parent, false);
+        return new MascotaViewHolder(v) ;
+    }
+
+    @Override
+    public void onBindViewHolder(final MascotaAdaptador.MascotaViewHolder holder, int position) {
+        final Mascota mascota = mascotas.get(position);
+        holder.imgFoto.setImageResource(mascota.getFoto());
+        holder.tvNombreCV.setText(mascota.getNombre());
+        holder.tvTantosCV.setText(Integer.toString(mascota.getTantos()));
+        holder.btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  Toast.makeText(activity, "Te gustaaaa  "+mascota.getNombre(), Toast.LENGTH_SHORT).show();
+                mascota.setTantos();
+                holder.tvTantosCV.setText(Integer.toString(mascota.getTantos()));
+                holder.tvTantosCV.invalidate();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mascotas.size();
+    }
+
+    public static class MascotaViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imgFoto;
+        private TextView tvNombreCV;
+        private ImageButton btnLike;
+        private TextView tvTantosCV;
+
+        public MascotaViewHolder(View itemView) {
+            super(itemView);
+            imgFoto = (ImageView) itemView.findViewById(R.id.imgFoto);
+            tvNombreCV = (TextView) itemView.findViewById(R.id.tvNombreCV);
+            btnLike = (ImageButton) itemView.findViewById(R.id.like);
+            tvTantosCV = (TextView) itemView.findViewById(R.id.tvTantoCV);
+        }
+    }
+}
